1f881ee515f1a550fd5ca85ae62e5733060e55ec
codingpartnerNB <nehab5314@gmail.com>
2023-08-06 17:07:45 +0530

    Understanding localStorage

---
+++ index.js
@@ -0,0 +1,47 @@
+// let btn = document.querySelector('.btn');
+// btn.addEventListener('click',(event)=>{
+//     event.preventDefault();
+//     document.querySelector('.myForm').style.backgroundColor='aqua';
+// });
+
+// btn.addEventListener('mouseover',(event)=>{
+//     event.preventDefault();
+//     document.querySelector('h1').style.backgroundColor = 'blue';
+//     document.querySelector('h1').style.color = 'white';
+// });
+
+// btn.addEventListener('mouseout',(event)=>{
+//     event.preventDefault();
+//     document.querySelector('h3').textContent = 'Hello';
+// });
+
+
+let myForm = document.querySelector('.myForm');
+let nameInput = document.querySelector('#name');
+let emailInput = document.querySelector('#email');
+let userDetail = document.querySelector('#userDetail');
+let msg = document.querySelector('.msg');
+
+myForm.addEventListener('submit',onSubmit);
+
+function onSubmit(event){
+    event.preventDefault();
+    if(nameInput.value === '' || emailInput.value === ''){
+        msg.classList.add('error');
+        msg.innerHTML = 'Please enter all fields';
+        setTimeout(()=>msg.remove(),2000);
+    }
+    else{
+        const li = document.createElement('li');
+        li.appendChild(document.createTextNode(`${nameInput.value} : ${emailInput.value}`));
+        userDetail.appendChild(li);
+
+        //Storing details in localStorage
+        localStorage.setItem('Name',nameInput.value);
+        localStorage.setItem('Email',emailInput.value);
+        
+        //clearing fields
+        nameInput.value = '';
+        emailInput.value = '';
+    }
+}
