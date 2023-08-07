fc1aa86bf1e32d326fa0d07b55fa441e3d43adee
codingpartnerNB <nehab5314@gmail.com>
2023-08-07 15:37:17 +0530

    Edit button added

--- index.js
+++ index.js
@@ -20,17 +20,26 @@ let myForm = document.querySelector('.myForm');
 let nameInput = document.querySelector('#name');
 let emailInput = document.querySelector('#email');
 let userDetail = document.querySelector('#userDetail');
-let msg = document.querySelector('.msg');
+// let msg = document.querySelector('.msg');
 
 myForm.addEventListener('submit', onSubmit);
 
 userDetail.addEventListener('click', deleteData);
 
+userDetail.addEventListener('click', editData);
+
 function onSubmit(event) {
     event.preventDefault();
     if (nameInput.value === '' || emailInput.value === '') {
-        msg.classList.add('error');
-        msg.innerHTML = 'Please enter all fields';
+        // msg.classList.add('error');
+        // msg.innerHTML = 'Please enter all fields';
+
+        // creating error element
+        let msg = document.createElement("div");
+        msg.className = "error";
+        msg.appendChild(document.createTextNode("Please enter all fields"));
+        let label = document.querySelector('label[for="name"]');
+        myForm.insertBefore(msg,label);
         setTimeout(() => msg.remove(), 2000);
     }
     else {
@@ -39,8 +48,13 @@ function onSubmit(event) {
         deleteBtn.setAttribute('type', 'button');
         deleteBtn.className = 'btn marginLeft delete';
         deleteBtn.setAttribute('value', 'Delete');
+        const editBtn = document.createElement('input');
+        editBtn.setAttribute('type','button');
+        editBtn.className = "btn marginLeft edit";
+        editBtn.setAttribute('value','Edit');   
         li.appendChild(document.createTextNode(`${nameInput.value}:${emailInput.value}`));
         li.appendChild(deleteBtn);
+        li.appendChild(editBtn);
         userDetail.appendChild(li);
 
         //Storing details in localStorage
@@ -71,12 +85,26 @@ function onSubmit(event) {
 function deleteData(event) {
     event.preventDefault();
     if (event.target.classList.contains('delete')) {
-        let keys = Object.keys(localStorage);
+        // Method 1
         let li = event.target.parentElement.textContent.split(":");
-        if (keys.includes(li[1])) {
-            localStorage.removeItem(li[1]);
-        }
+        localStorage.removeItem(li[1]);
+        // Method 2
+        // let keys = Object.keys(localStorage);
+        // let li = event.target.parentElement.textContent.split(":");
+        // if (keys.includes(li[1])) {
+        //     localStorage.removeItem(li[1]);
+        // }
         let listIt = event.target.parentElement;
         userDetail.removeChild(listIt);
+    }
+}
+
+function editData(event){
+    if(event.target.classList.contains('edit')){
+        let li = event.target.parentElement.textContent.split(":");
+        nameInput.value = li[0];
+        emailInput.value = li[1];
+        localStorage.removeItem(li[1]);
+        userDetail.removeChild(event.target.parentElement);
     }
 }
